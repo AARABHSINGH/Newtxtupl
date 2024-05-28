1bd21465593f542dc3a79efcb125c2a2f4448c38
AARABHSINGH <168511707+AARABHSINGH@users.noreply.github.com>
2024-05-28 18:25:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'ʟʊʍɨռǟռȶ Txt Bot 2'
+    return 'GURJAR Txt Bot 2'
 
 
-if __name__ == "ʟʊʍɨռǟռȶ Txt Bot 2":
+if __name__ == "GURJAR Txt Bot 2":
     app.run()
