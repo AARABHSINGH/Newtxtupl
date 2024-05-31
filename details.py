4b490fd5073aec781f575304899c2ded002be964
AARABHSINGH <168511707+AARABHSINGH@users.noreply.github.com>
2024-05-31 21:03:48 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
 api_id =  25434657
 api_hash = "22cfc54f94cf17360dc1475a51e38518"
-bot_token = "7335464967:AAE_B77yuVtupfTBVLOmbd6F1KApjsR0W9E"
-log_channel = -1002149340418
+bot_token = "7429923046:AAGwGgzYJDmT3v8KGAVTQVFwPk6Wyab48N8"
+log_channel = -1002209397514
 
