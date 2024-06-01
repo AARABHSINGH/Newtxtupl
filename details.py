f2ddaf1cdce61e4e46d15d029cbc01dcc59567ee
AARABHSINGH <168511707+AARABHSINGH@users.noreply.github.com>
2024-06-01 21:19:53 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
 api_id =  25434657
 api_hash = "22cfc54f94cf17360dc1475a51e38518"
-bot_token = "7429923046:AAGwGgzYJDmT3v8KGAVTQVFwPk6Wyab48N8"
+bot_token = "6905499731:AAGXqmNao3J0K0PNne1cJqZdddypOJZL6Mc"
 log_channel = -1002209397514
 
