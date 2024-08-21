3f9605da6a88a28444ea58b041f9a5ad46c64cc3
AARABHSINGH <168511707+AARABHSINGH@users.noreply.github.com>
2024-08-21 16:20:37 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
-api_id =  25434657
-api_hash = "22cfc54f94cf17360dc1475a51e38518"
-bot_token = "7262948482:AAGlScUTtXBuwfUX6KW3OP9D5pPxGF1FXJw"
-log_channel = -1002209397514
+api_id =  _
+api_hash = "__"
+bot_token = "__"
+log_channel = -__
 
